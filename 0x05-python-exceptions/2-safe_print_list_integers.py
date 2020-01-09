1b5052f301092d16e1b50eecb125dfe3f90c33fb
Jeremy Bedolla <jjbedolla@gmail.com>
2020-01-09 02:18:10 +0000

    fixed tpy0

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,9 +1,13 @@
 #!/usr/bin/python3
+
+
 def safe_print_list_integers(my_list=[], x=0):
+
     c = 0
+
     for i in range(x):
         try:
-            print("{:d}".gormat(my_list[i]), end="")
+            print("{:d}".format(my_list[i]), end="")
             c = c + 1
         except (ValueError, TypeError):
             continue
