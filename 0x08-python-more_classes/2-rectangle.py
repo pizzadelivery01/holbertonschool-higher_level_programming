fc98e903d33ed75806131cfbd8d63a1e24e3b09d
Jeremy Bedolla <jjbedolla@gmail.com>
2020-01-14 14:02:44 +0000

    updating functions

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -31,9 +31,21 @@ class Rectangle:
 
     @height.setter
     def height(self, value):
-        """setter:height"""
+        """setter: height"""
         if type(value) is not int:
             raise TypeError("height must be an integer")
         if value < 0:
             raise ValueError("height must be >= 0")
         self.__height = value
+
+    def area(self):
+        """ method: area"""
+        if self.__width and self.__height:
+            return (self.__width * self.__height)
+
+    def perimeter(self):
+        """method: perimeter"""
+        if self.__width == 0 or self.__height == 0:
+            return 0
+        else
+        return (self.__width * 2) + (self.__height * 2)
