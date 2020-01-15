3d1cb45ab25165143a25529c1fe7e70b5f1c48df
Jeremy Bedolla <jjbedolla@gmail.com>
2020-01-15 05:28:55 +0000

    fixed indent

--- 0x08-python-more_classes/6-rectangle.py
+++ 0x08-python-more_classes/6-rectangle.py
@@ -7,7 +7,7 @@ class Rectangle:
     """class: Rectangle"""
 
 
-number_of_instances = 0
+    number_of_instances = 0
 
     def __init__(self, width=0, height=0):
         """init: self, width, height"""
