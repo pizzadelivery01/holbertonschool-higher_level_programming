0441ef4a1ddda75ebed077257a99adae1e4ccd48
Jeremy Bedolla <jjbedolla@gmail.com>
2020-01-15 05:06:28 +0000

    fixed else

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -47,5 +47,5 @@ class Rectangle:
         """method: perimeter"""
         if self.__width == 0 or self.__height == 0:
             return 0
-        else
-        return (self.__width * 2) + (self.__height * 2)
+        else:
+            return (self.__width * 2) + (self.__height * 2)
