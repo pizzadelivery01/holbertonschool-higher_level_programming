eb176b25906c00a03d02f621be33932e95248577
Jeremy Bedolla <jjbedolla@gmail.com>
2020-01-16 08:15:22 +0000

    added newline pep8

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -18,6 +18,7 @@ class Rectangle:
         if height < 0:
             raise ValueError("height must be >= 0")
         self.__height = height
+
     @property
     def width(self):
         """getter: width"""
