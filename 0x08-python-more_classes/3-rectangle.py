80a41aae8d67d726e0abd4ee0696bf9d09c7bc37
Jeremy Bedolla <jjbedolla@gmail.com>
2020-01-16 08:08:38 +0000

    added type and value errors

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -7,9 +7,17 @@ class Rectangle:
     """class: Rectangle"""
     def __init__(self, width=0, height=0):
         """init: self, width, height"""
+         if type(width) is not int:
+            raise TypeError("width must be an integer")
+        if width < 0:
+            raise ValueError("width must be >= 0")
         self.__width = width
-        self.__height = height
 
+        if type(height) is not int:
+            raise TypeError("height must be an integer")
+        if height < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = height
     @property
     def width(self):
         """getter: width"""
