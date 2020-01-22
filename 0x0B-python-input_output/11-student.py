5cca278a40e9b22a1fd2366a6ffe5c4ef138cb09
Jeremy Bedolla <jjbedolla@gmail.com>
2020-01-22 04:28:27 +0000

    adding files

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+create class and retrieves dict
+"""
+
+
+class Student:
+    """class Student"""
+    def __init__(self, first_name, last_name, age):
+        """init class"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """return dict"""
+        return self.__dict__
