3c0b02d93987fcd05a36ac164224b751c67569a3
Jeremy Bedolla <jjbedolla@gmail.com>
2020-01-22 04:10:12 +0000

    switched logic

--- 0x0B-python-input_output/9-add_item.py
+++ 0x0B-python-input_output/9-add_item.py
@@ -8,9 +8,10 @@ import sys
 save = __import__('7-save_to_json_file').save_to_json_file
 load = __import__('8-load_from_json_file').load_from_json_file
 
-my_list = []
+new = "add_item.json"
 
-for i in range(1, len(sys.argv)):
-    my_list.append(sys.argv[i])
-load(my_list, "add_itme.json")
-save(my_list, "add_item.json")
+try:
+    my_list = load(new)
+    save(my_list + argv[1:], new)
+except:
+    save([] + argv[1:], new)
