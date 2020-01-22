6914faaf500f20de1aaa2b488b83aace1c07a2eb
Jeremy Bedolla <jjbedolla@gmail.com>
2020-01-22 04:17:35 +0000

    changed argv to sys.argv

--- 0x0B-python-input_output/9-add_item.py
+++ 0x0B-python-input_output/9-add_item.py
@@ -9,9 +9,9 @@ save = __import__('7-save_to_json_file').save_to_json_file
 load = __import__('8-load_from_json_file').load_from_json_file
 
 new = "add_item.json"
-
+my_list = []
 try:
     my_list = load(new)
-    save(my_list + argv[1:], new)
+    save(my_list + sys.argv[1:], new)
 except:
-    save([] + argv[1:], new)
+    save([] + sys.argv[1:], new)
