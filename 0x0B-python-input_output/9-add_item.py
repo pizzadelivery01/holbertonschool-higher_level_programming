9e2282b82bc4ac4c35f9cf31eefb4f8d0be73f83
Jeremy Bedolla <jjbedolla@gmail.com>
2020-01-22 04:01:06 +0000

    adding changes

--- 0x0B-python-input_output/9-add_item.py
+++ 0x0B-python-input_output/9-add_item.py
@@ -8,10 +8,9 @@ import sys
 save = __import__('7-save_to_json_file').save_to_json_file
 load = __import__('8-load_from_json_file').load_from_json_file
 
-try:
-    my_list = load("add_item.json")
-except:
-    my_list = []
+my_list = []
+
 for i in range(1, len(sys.argv)):
     my_list.append(sys.argv[i])
+load(my_list, "add_itme.json")
 save(my_list, "add_item.json")
