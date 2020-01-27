28c66fc1b3b8a32b2534fd51917778b558de628e
Jeremy Bedolla <jjbedolla@gmail.com>
2020-01-27 22:53:01 +0000

    adding files

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -10,10 +10,26 @@ class Rectangle(Base):
     """a Rectangle"""
     def __init__(self, width, height, x=0, y=0, id=None):
         """initializes"""
-        self.width = width
+        if type(width) is not int:
+            raise TypeError("width must be an integer")
+        if width <= 0:
+            raise ValueError("width must be > 0")
+        self.__width = width
+        if type(height) is not int:
+            raise TypeError("height must be an integer")
+        if height <= 0:
+            raise ValueError("height must be > 0")
         self.height = height
-        self.x = x
+        if type(y) is not int:
+            raise TypeError("y must be an integer")
+        if y < 0:
+            raise ValueError("y must be >= 0")
         self.y = y
+        if type(x) is not int:
+            raise TypeError("x must be an integer")
+        if x < 0:
+            raise ValueError("x must be >= 0")
+        self.__x = x
         super().__init__(id)
 
         @property
