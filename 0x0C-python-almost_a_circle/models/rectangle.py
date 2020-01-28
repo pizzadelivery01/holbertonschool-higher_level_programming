ef94b21f2698715337fbadf3442f9b5d30752586
Jeremy Bedolla <jjbedolla@gmail.com>
2020-01-28 01:16:09 +0000

    changed the typeerrors and moved super()

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -10,27 +10,11 @@ class Rectangle(Base):
     """a Rectangle"""
     def __init__(self, width, height, x=0, y=0, id=None):
         """initializes"""
-        if type(width) is not int:
-            raise TypeError("width must be an integer")
-        if width <= 0:
-            raise ValueError("width must be > 0")
-        self.width = width
-        if type(height) is not int:
-            raise TypeError("height must be an integer")
-        if height <= 0:
-            raise ValueError("height must be > 0")
-        self.height = height
-        if type(y) is not int:
-            raise TypeError("y must be an integer")
-        if y < 0:
-            raise ValueError("y must be >= 0")
-        self.y = y
-        if type(x) is not int:
-            raise TypeError("x must be an integer")
-        if x < 0:
-            raise ValueError("x must be >= 0")
-        self.x = x
         super().__init__(id)
+        self.width = width
+        self.height = height
+        self.y = y
+        self.x = x
 
         @property
         def width(self):
