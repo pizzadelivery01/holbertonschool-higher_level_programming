b7710ce56a24df10597dbb715a61dd1e5d5c70ba
Jeremy Bedolla <jjbedolla@gmail.com>
2020-03-03 22:49:12 -0500

    adding first batch

---
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -0,0 +1,5 @@
+-- lists all privileges of the MySQL users user_0d_1 and user_0d_2
+-- show priviledges for user1
+SHOW GRANTS FOR user_0d_1@localhost;
+-- show priviledges for user2
+SHOW GRANTS FOR user_0d_2@localhost;
---
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -0,0 +1,7 @@
+-- creates the MySQL server user user_0d_1
+-- create user
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+-- grant privs
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
+-- flush
+FLUSH PRIVILEGES;
