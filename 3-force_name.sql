5efc0d243ef986fb0577bfd141d1f8ed704765c7
Jeremy Bedolla <jjbedolla@gmail.com>
2020-03-03 22:54:52 -0500

    wrong directory:

--- 3-force_name.sql
+++
@@ -1,6 +0,0 @@
--- creates the table force_name on your MySQL server. with ID name
--- create table
-CREATE TABLE IF NOT EXISTS force_name (
-    id INT,
-    name VARCHAR(256) NOT NULL
-) ENGINE=INNODB;
