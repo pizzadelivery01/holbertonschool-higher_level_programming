46a06ba322794734c98ba98ccec6ba782839401a
Jeremy Bedolla <jjbedolla@gmail.com>
2020-03-28 17:32:07 -0400

    forgot quotes around execute statement

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -10,7 +10,7 @@ if __name__ == "__main__":
     db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
                          passwd=argv[2], db=argv[3])
     cursor = db.cursor()
-    cursor.execute(SELECT * from states WHERE name like "N%" ORDER BY id ASC)
+    cursor.execute("SELECT * from states WHERE name like 'N%' ORDER BY id ASC")
     rows = cursor.fetchall()
     for each in rows:
         print(each)
