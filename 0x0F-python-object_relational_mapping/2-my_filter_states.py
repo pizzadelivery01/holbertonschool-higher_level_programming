34a00149e6f209c002391f343bf5425a6740f07a
Jeremy Bedolla <jjbedolla@gmail.com>
2020-03-28 16:21:32 -0400

    pep8 fixes

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -11,7 +11,7 @@ if __name__ == "__main__":
                          passwd=argv[2], db=argv[3])
     cursor = db.cursor()
     cursor.execute("SELECT * FROM states WHERE name LIKE '{:s}'
-    ORDER BY id ASC".format(argv[4],))
+                   ORDER BY id ASC".format(argv[4],))
     rows = cursor.fetchall()
     for row in rows:
         if row[1] == argv[4]:
--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -10,8 +10,9 @@ if __name__ == "__main__":
     db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
                          passwd=argv[2], db=argv[3])
     cursor = db.cursor()
-    cursor.execute("SELECT cities.id, cities.name, states.name
-    FROM cities JOIN states ON cities.state_id = states.id ORDER BY cities.id")
+    cursor.execute("SELECT cities.id, cities.name, states.name FROM cities \
+                   JOIN states ON cities.state_id = states.id \
+                   ORDER BY cities.id")
     rows = cursor.fetchall()
     for row in rows:
         print(row)
