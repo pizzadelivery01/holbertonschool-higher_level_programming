05b3ba096edf265574f668a6cf5268631ab65b0d
Jeremy Bedolla <jjbedolla@gmail.com>
2020-03-29 00:08:25 -0400

    pep8

--- 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -10,15 +10,16 @@ from sys import argv
 from model_state import Base, State
 
 if __name__ == "__main__":
-        eng = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
-                argv[1],
-                argv[2],
-                argv[3],))
-        Base.metadata.create_all(eng)
-        Session = sessionmaker(bind=eng)
-        session = Session()
-        match = '%a%'
-        states = session.query(State).filter(State.name.like(match)).order_by(State.id)
-        for state in states:
-            print("{}: {}".format(state.id, state.name))
-        session.close()
+    eng = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+        argv[1],
+        argv[2],
+        argv[3],))
+    Base.metadata.create_all(eng)
+    Session = sessionmaker(bind=eng)
+    session = Session()
+    match = '%a%'
+    states = session.query(State).filter(
+        State.name.like(match)).order_by(State.id)
+    for state in states:
+        print("{}: {}".format(state.id, state.name))
+    session.close()
