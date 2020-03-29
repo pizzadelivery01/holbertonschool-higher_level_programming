e871eb68cc278b878c5c80d5cd3639d32c443bd3
Jeremy Bedolla <jjbedolla@gmail.com>
2020-03-29 00:05:27 -0400

    pep8 fix

--- 0x0F-python-object_relational_mapping/11-model_state_insert.py
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -11,9 +11,10 @@ from model_state import Base, State
 
 
 if __name__ == "__main__":
-        eng = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(argv[1],
-                                                                        argv[2],
-                                                                        argv[3]))
+        eng = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+                argv[1],
+                argv[2],
+                argv[3]))
         Base.metadata.create_all(eng)
         Session = sessionmaker(bind=eng)
         session = Session()
