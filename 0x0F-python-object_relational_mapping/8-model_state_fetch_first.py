9b32381ecee38b45f30e652da0cb9e6f9fbdea64
Jeremy Bedolla <jjbedolla@gmail.com>
2020-03-29 00:16:52 -0400

    got rid of order by

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -18,7 +18,7 @@ if __name__ == "__main__":
     Base.metadata.create_all(eng)
     Session = sessionmaker(bind=eng)
     session = Session()
-    first_state = session.query(State).order_by(State.id).first()
+    first_state = session.query(State).first()
     if first_state is not None:
         print("{}: {}".format(first_state.id, first_state.name))
     else:
