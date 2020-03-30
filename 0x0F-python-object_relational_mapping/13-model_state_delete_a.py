8a465d2cbf8cd918df2c5c4b5925ec234bd3a499
Jeremy Bedolla <jjbedolla@gmail.com>
2020-03-29 22:13:39 -0400

    pep8

--- 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
- deletes all State objects with a name containing the letter a from the database 
+deletes all States with a name containing the letter a from the database
 """
 
 import sqlalchemy
