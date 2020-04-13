c99f48a8a8a488dc893dfda09c3ac428d0dc957d
Jeremy Bedolla <jjbedolla@gmail.com>
2020-04-13 17:28:26 -0400

    variable

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -8,4 +8,4 @@ if __name__ == "__main__":
     req = requests.get("https://intranet.hbtn.io/status")
     print("Body response:")
     print("\t- type:{}".format(type(req.text)))
-    print("\t- content: {}".format(r.text))
+    print("\t- content: {}".format(req.text))
