99c5298bbd6e0bf57c64a520161b04987dca6461
Jeremy Bedolla <jjbedolla@gmail.com>
2020-04-13 15:52:42 -0400

    adding 3

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+ a Python script that takes in a URL, sends a request
+ to the URL and displays the body of the response
+ and handle httpError
+"""
+
+if __name__ == "__main__":
+    import urllib.error
+    import urllib.request
+    from sys import argv
+    req = urllib.request.Request(argv[1])
+    try:
+        with urllib.request.urlopen(req) as r:
+            print(r.read().decode('utf-8'))
+    except error.HTTPError as e:
+        print("Error code: {}".format(e.code)
