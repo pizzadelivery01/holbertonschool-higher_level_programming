3fa5f0023e79c675b594e2741bb7153b995c0beb
Jeremy Bedolla <jjbedolla@gmail.com>
2020-04-13 14:16:35 -0400

    added import guard and separated print statments

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -4,7 +4,10 @@ gets status with urllib
 """
 import urllib
 
-with urllib.request.urlopen("https://intranet.hbtn.io/status") as req:
-    response = req.read()
-    print("Body response:\n\t- type: {}\n\t- content: {}\n\t- utf8 content: {}"
-          .format(type(response), response, response.decode('utf8')))
+if __name__ == "__main__":
+    with urllib.request.urlopen("https://intranet.hbtn.io/status") as req:
+        response = req.read()
+        print("Body response:")
+        print("\t- type: {}".format(type(response)))
+        print("\t- content: {}".format(response))
+        print("\t- utf8 content: {}".format(respose.decode('utf8')))
