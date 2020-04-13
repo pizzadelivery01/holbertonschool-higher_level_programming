026b3ea5305a8885e532b07b914f483bb58b85fd
Jeremy Bedolla <jjbedolla@gmail.com>
2020-04-13 17:07:02 -0400

    revert

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -4,11 +4,13 @@
  to the URL and displays the body of the response
  and handle httpError
 """
-from urllib import error, request, parse
+from urllib import error as error
+from urllib import request as request
 
 if __name__ == "__main__":
+    req = request.Request(argv[1])
     try:
-        with request.urlopen(argv[1]) as r:
+        with request.urlopen(req) as r:
             print(r.read().decode('utf-8'))
     except error.HTTPError as e:
         print("Error code: {}".format(e.code))
