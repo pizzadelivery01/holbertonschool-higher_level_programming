817e09c7b993ee309bd754450bd1ab9c30fe1c2d
Jeremy Bedolla <jjbedolla@gmail.com>
2020-04-13 18:27:29 -0400

    pep8 and get add

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 """
-Python script that takes in a letter and sends 
-a POST request to http://0.0.0.0:5000/search_user 
+Python script that takes in a letter and sends
+a POST request to http://0.0.0.0:5000/search_user
 with the letter as a parameter.
 
 The letter must be sent in the variable q
 If no argument is given, set q=""
-If the response body is properly JSON formatted and 
+If the response body is properly JSON formatted and
 not empty, display the id and name like this: [<id>] <name>
 
 Otherwise:
@@ -19,17 +19,18 @@ You are not allowed to import packages other than requests and sys
 if __name__ == "__main__":
     import requests
     from sys import argv
-    if len(argv) > 1:
+    if len(argv) == 2:
         q = argv[1]
     else:
         q = ""
     r = requests.post("http://0.0.0.0:5000/search_user", data={'q': q})
     try:
         json_dict = r.json()
+        
         if not json_dict:
             print("No result")
         else:
-            print('[{}] {}'.format(json_dict['id'],
-                                   json_dict['name']))
+            print('[{}] {}'.format(json_dict.get['id'],
+                                   json_dict.get['name']))
     except ValueError:
         print("Not a valid JSON")
