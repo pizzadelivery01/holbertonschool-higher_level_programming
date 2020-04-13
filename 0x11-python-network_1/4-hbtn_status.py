494f8648c718a585e577eac39e7ceb89f775161c
Jeremy Bedolla <jjbedolla@gmail.com>
2020-04-13 17:27:02 -0400

    adding

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+a Python script that fetches https://intranet.hbtn.io/status
+"""
+if __name__ == "__main__":
+    import requests
+
+    req = requests.get("https://intranet.hbtn.io/status")
+    print("Body response:")
+    print("\t- type:{}".format(type(req.text)))
+    print("\t- content: {}".format(r.text))
