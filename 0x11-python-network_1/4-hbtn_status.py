d1de20edf5635df3686141186d44f1d45b5e3a6c
Jeremy Bedolla <jjbedolla@gmail.com>
2020-04-13 17:47:07 -0400

    mising space

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -7,5 +7,5 @@ if __name__ == "__main__":
 
     req = requests.get("https://intranet.hbtn.io/status")
     print("Body response:")
-    print("\t- type:{}".format(type(req.text)))
+    print("\t- type: {}".format(type(req.text)))
     print("\t- content: {}".format(req.text))
