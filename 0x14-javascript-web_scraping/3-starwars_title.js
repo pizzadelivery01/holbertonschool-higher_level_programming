de1e3dbd2d1a169db83abde8dd15ea5afbfd1112
Jeremy Bedolla <jjbedolla@gmail.com>
2020-05-12 16:23:46 -0400

    ===

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -4,9 +4,9 @@ const request = require('request');
 const url = 'https://swapi-api.hbtn.io/api/films/';
 const episode = process.argv[2];
 request(url + episode, function (err, response, body) {
-    if (err) {
+  if (err) {
     console.log(err);
-  } else if (response.statusCode == 200) {
+  } else if (response.statusCode === 200) {
     const obj = JSON.parse(body);
     console.log(obj.title);
   } else {
