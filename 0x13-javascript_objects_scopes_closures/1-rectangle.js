6338b4e030fb0bc1ad47a2338844eec38142a17d
Jeremy Bedolla <jjbedolla@gmail.com>
2020-05-11 20:57:51 -0400

    semistandard

--- 0x13-javascript_objects_scopes_closures/1-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 // rectangle
 module.exports = class Rectangle {
-    constructor (w, h) {
-	this.width = w;
-	this.height = h;
-    }
+  constructor (w, h) {
+    this.width = w;
+    this.height = h;
+  }
 };
