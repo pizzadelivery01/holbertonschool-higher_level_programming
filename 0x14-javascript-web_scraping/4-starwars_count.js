2241a04d602ea340b57dd9b6e7e3856619fe73f1
Jeremy Bedolla <jjbedolla@gmail.com>
2020-05-12 23:39:17 -0400

    semistandard

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -3,21 +3,21 @@
 const request = require('request');
 const url = process.argv[2];
 request(url, function (err, response, body) {
-    if (err) {
-	console.log(err);
-    } else if (response.statusCode === 200) {
-	let films = JSON.parse(body).results;
-	let count = 0;
-	for (let movies in films) {
-	    let chars = films[movies].characters;
-	    for (let index in Chars) {
-		if (Chars[index].includes('18')) {
-		    count++;
-		}
-	    }
-	}
-	console.log(count);
-    } else {
-	console.log('An error occured. Status code: ' + response.statusCode);
+  if (err) {
+    console.log(err);
+  } else if (response.statusCode === 200) {
+    const films = JSON.parse(body).results;
+    let count = 0;
+    for (const movies in films) {
+      const chars = films[movies].characters;
+      for (const index in chars) {
+        if (chars[index].includes('18')) {
+          count++;
+        }
+      }
     }
+    console.log(count);
+  } else {
+    console.log('An error occured. Status code: ' + response.statusCode);
+  }
 });
