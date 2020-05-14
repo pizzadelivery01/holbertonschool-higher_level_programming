29a409501b70d37b5f942be5b2874eb928204c0a
Jeremy Bedolla <jjbedolla@gmail.com>
2020-05-14 19:00:55 -0400

    updated script to remove shebang add quotes

--- 0x15-javascript-web_jquery/0-script.js
+++ 0x15-javascript-web_jquery/0-script.js
@@ -1,5 +1,2 @@
-#!/usr/bin/node
-// updates HEADER tag to red
-(function change() {
-document.querySelector(header).style.color = '#FF0000';
-});    
+// update header to red
+document.querySelector('header').style.color = '#FF0000';
